83a03b70bb2f1066b4d41f7cee9d46e345aeaa40
ubaidori <imubaidori@gmail.com>
2026-05-19 16:28:35 +0700

    add dashboard layout after login

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
+const loginScreen = document.getElementById("loginScreen");
+const appScreen = document.getElementById("appScreen");
 const loginForm = document.getElementById("loginForm");
+
+const openSidebarButton = document.getElementById("openSidebar");
+const sidebar = document.getElementById("sidebar");
+const sidebarOverlay = document.getElementById("sidebarOverlay");
 
 if (loginForm) {
   loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    alert("Login simulasi berhasil. Selanjutnya masuk ke Dashboard Ustadz.");
+
+    loginScreen.classList.add("hidden");
+    appScreen.classList.remove("hidden");
+  });
+}
+
+if (openSidebarButton && sidebar && sidebarOverlay) {
+  openSidebarButton.addEventListener("click", function () {
+    sidebar.classList.remove("-translate-x-full");
+    sidebarOverlay.classList.remove("hidden");
+  });
+
+  sidebarOverlay.addEventListener("click", function () {
+    sidebar.classList.add("-translate-x-full");
+    sidebarOverlay.classList.add("hidden");
   });
 }
